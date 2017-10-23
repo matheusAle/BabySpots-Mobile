f8f3a91d9304984df9bb749a207bf21313f2a804
TalissaSilva <32316693+TalissaSilva@users.noreply.github.com>
2017-10-23 15:41:42 -0300

    Update Estabelecimento.java

--- app/src/main/java/com/facom/progmob/kmrt/babyspots/model/Estabelecimento.java
+++ app/src/main/java/com/facom/progmob/kmrt/babyspots/model/Estabelecimento.java
@@ -7,5 +7,25 @@ package com.facom.progmob.kmrt.babyspots.model;
 public class Estabelecimento {
     private String nome;
     private String endreco;
+    
+    public Estabelecimento(String nome, String endereco) {
+        this.nome = nome;
+        this.endereco = endereco;
+    }
+    
+    public String getNome() {
+        return nome;
+    }
 
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }    
 }
