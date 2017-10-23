066dc852b5b550345168ac31318960a4a10dd330
TalissaSilva <32316693+TalissaSilva@users.noreply.github.com>
2017-10-23 15:42:23 -0300

    Update Usuario.java

--- app/src/main/java/com/facom/progmob/kmrt/babyspots/model/Usuario.java
+++ app/src/main/java/com/facom/progmob/kmrt/babyspots/model/Usuario.java
@@ -15,5 +15,34 @@ public class Usuario {
     private Image fotoDePerfil;
 
     //TODO continuar a implementação!
+    public Usuario(String nome, String email, Image fotoDePerfil) {
+        this.nome = nome;
+        this.email = email;
+        this.fotoDePerfil = fotoDePerfil;
+    }
+    
+    
+    public String getNome() {
+        return nome;
+    }
 
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Image getFotoDePerfil() {
+        return fotoDePerfil;
+    }
+
+    public void setFotoDePerfil(Image fotoDePerfil) {
+        this.fotoDePerfil = fotoDePerfil;
+    }
 }
